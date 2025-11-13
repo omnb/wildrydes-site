27794ec59e7222b12a399eb6f0f17e93757332db
omnb <96018039+omnb@users.noreply.github.com>
2025-11-13 11:07:10 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_hwB6vljiE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '53m36odbhu0l7n8q089tnkdcot', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YvVnob7rU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'i3tqp0hl92n6uf10fuqqqdgbe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
